38c8e0707e4b76cc3eb34fde64db27e0e11467c4
mari <marianaribenova@hotmail.com>
2024-11-21 16:45:12 -0300

    botão e criação de estados

--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { View, Text, TouchableOpacity} from "react-native"
 import { MaterialIcons } from "@expo/vector-icons"
 import { router } from "expo-router"
@@ -7,8 +8,15 @@ import { colors } from "@/styles/colors"
 
 import { Categories } from "@/components/categories"
 import { Input } from "@/components/input"
+import { Button } from "@/components/button"
 
 export default function Add(){
+    const[name, setName]= useState("")
+    const[url, setUrl]= useState("")
+
+    function handleAdd(){
+        console.log({name,url})
+    }
     return(
         <View style={styles.container}>
             <View style={styles.header}>
@@ -22,9 +30,11 @@ export default function Add(){
             <Categories/>
 
             <View style={styles.form}>
-              <Input placeholder="Nome"/>
-              <Input placeholder="Url"/>
+              <Input placeholder="Nome" onChangeText={setName} autoCorrect={false}/>
+              <Input placeholder="Url" onChangeText={setUrl} autoCorrect={false}/>
+              <Button title= "Adicionar" onPress={handleAdd}/>
             </View>
+
         </View>
     )
 }
